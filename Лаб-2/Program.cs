fb452b00cf62071e8c6ed57529c3456b79bca741
Aleksandr l <boss.startov@bk.ru>
2021-10-07 14:13:40 +0200

    Добавьте файлы проекта.

---
+++ Лаб-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Лаб_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Высокий уровень - 8 вариант
+                Console.WriteLine("Введите первый катет");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второй катет");
+                double c;
+                double b = double.Parse(Console.ReadLine());
+                c = Math.Sqrt(Math.Pow(a, 2) * (Math.Pow(b, 2)));
+                Console.WriteLine("Гипотенуза равна {0}", c);
+                double S = (a * b / 2);
+                Console.WriteLine("Площадь равна {0}", S);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введите корректно перменную");
+            }   
+        }       
+    }
+}
